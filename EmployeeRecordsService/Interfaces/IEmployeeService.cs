ab64d9b2974cfeb55f8bf876da60c6fc90a7b8ce
Adrianne Claude Tubig <art0901@carenethealthcare.com>
2021-06-13 00:14:53 +0800

    Initial Service project

---
+++ EmployeeRecordsService/Interfaces/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using EmployeeRecordsDomain.Dto;
+using EmployeeRecordsDomain.Entities;
+using EmployeeRecordsRepository.Interfaces;
+using System;
+
+namespace EmployeeRecordsService.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Result<long> Create(EmployeeCreateDto employeeCreateDto);
+    }
+}
---
+++ EmployeeRecordsService/Services/EmployeeService.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+using EmployeeRecordsDomain.Dto;
+using EmployeeRecordsDomain.Entities;
+using EmployeeRecordsRepository.Interfaces;
+using EmployeeRecordsService.Interfaces;
+using System;
+
+namespace EmployeeRecordsService.Services
+{
+    public sealed class EmployeeService: IEmployeeService
+    {
+        private readonly IEmployeeRepository _iEmployeeRepository;
+
+        public EmployeeService(IEmployeeRepository iEmployeeRepository)
+        {
+            if (iEmployeeRepository == null)
+                throw new ArgumentNullException("IEmployeeRepository Required");
+
+            _iEmployeeRepository = iEmployeeRepository;
+        }
+
+        public Result<long> Create(EmployeeCreateDto employeeCreateDto)
+        {
+            var employeeResult = Employee.Create(employeeCreateDto);
+            if (employeeResult.IsFailure)
+                return Result.Failure<long>(employeeResult.Error);
+
+            var employeeId = _iEmployeeRepository.Create(employeeResult.Value);
+
+            return Result.Success(employeeId);
+        }
+    }
+}
